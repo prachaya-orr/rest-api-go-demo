50de80425a388f7a946463a5ba3c77fd0aab7cc4
Prachaya Orraeiam <prachaya.or@bdms.co.th>
2023-12-21 14:26:54 +0700

    add logger middleware

--- fiber-api/server.go
+++ fiber-api/server.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	fiber "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	app := fiber.New()
-
+	app.Use(logger.New())
 	app.Get("/books", getBooks)
 	app.Get("/books/:id", getBookById)
 	app.Post("/books", createBook)
