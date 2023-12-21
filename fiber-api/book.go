874e0f8abc38dc94db31f639076a34025a676ac4
Prachaya Orraeiam <prachaya.or@bdms.co.th>
2023-12-21 14:23:29 +0700

    modify file

--- fiber-api/book.go
+++ fiber-api/book.go
@@ -6,69 +6,88 @@ import (
 	fiber "github.com/gofiber/fiber/v2"
 )
 
+// Book struct to hold book data
+type Book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+// Initialize in-memory data
+var books []Book = []Book{
+	{ID: 1, Title: "1984", Author: "George Orwell"},
+	{ID: 2, Title: "The Great Gatsby", Author: "F. Scott Fitzgerald"},
+}
+
+// Handler functions
+
 func getBooks(c *fiber.Ctx) error {
 	return c.JSON(books)
 }
 
 func getBookById(c *fiber.Ctx) error {
-	bookId, err := strconv.Atoi(c.Params("id"))
-
+	id, err := strconv.Atoi(c.Params("id"))
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+		return c.SendStatus(fiber.StatusBadRequest)
 	}
 
 	for _, book := range books {
-		if bookId == book.ID {
+		if book.ID == id {
 			return c.JSON(book)
 		}
 	}
 
-	return c.Status(fiber.StatusNotFound).SendString("Book not found")
+	return c.SendStatus(fiber.StatusNotFound)
 }
 
 func createBook(c *fiber.Ctx) error {
 	book := new(Book)
+
 	if err := c.BodyParser(book); err != nil {
 		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
 	}
+
+	book.ID = len(books) + 1
 	books = append(books, *book)
+
 	return c.JSON(book)
 }
 
 func updateBook(c *fiber.Ctx) error {
-	bookId, err := strconv.Atoi(c.Params("id"))
-
+	id, err := strconv.Atoi(c.Params("id"))
 	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	updatedBook := new(Book)
+	if err := c.BodyParser(updatedBook); err != nil {
 		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
 	}
 
-	bookUpdate := new(Book)
-
-	if err := c.BodyParser(bookUpdate); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
-	}
 	for i, book := range books {
-		if book.ID == bookId {
-			books[i].Title = bookUpdate.Title
-			books[i].Author = bookUpdate.Author
-			return c.JSON(books[i])
+		if book.ID == id {
+			book.Title = updatedBook.Title
+			book.Author = updatedBook.Author
+			books[i] = book
+			return c.JSON(book)
 		}
 	}
-	return c.Status(fiber.StatusNotFound).SendString("Book not found")
+
+	return c.SendStatus(fiber.StatusNotFound)
 }
 
 func deleteBook(c *fiber.Ctx) error {
-	bookId, err := strconv.Atoi(c.Params("id"))
-
+	id, err := strconv.Atoi(c.Params("id"))
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+		return c.SendStatus(fiber.StatusBadRequest)
 	}
 
 	for i, book := range books {
-		if book.ID == bookId {
+		if book.ID == id {
 			books = append(books[:i], books[i+1:]...)
 			return c.SendStatus(fiber.StatusNoContent)
 		}
 	}
-	return c.Status(fiber.StatusNotFound).SendString("Book not found")
+
+	return c.SendStatus(fiber.StatusNotFound)
 }
--- fiber-api/server.go
+++ fiber-api/server.go
@@ -4,19 +4,8 @@ import (
 	fiber "github.com/gofiber/fiber/v2"
 )
 
-type Book struct {
-	ID     int    `json:"id"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
-}
-
-var books []Book
-
 func main() {
 	app := fiber.New()
-
-	books = append(books, Book{ID: 1, Title: "Golang pointers", Author: "Mr. Golang"})
-	books = append(books, Book{ID: 2, Title: "Goroutines", Author: "Mr. Goroutine"})
 
 	app.Get("/books", getBooks)
 	app.Get("/books/:id", getBookById)
