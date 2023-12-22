d7f75d1f7bce0d0e52629c933d5d23307ba70db0
Prachaya Orraeiam <prachaya.or@bdms.co.th>
2023-12-22 07:27:58 +0700

    Upload File

--- fiber-api/server.go
+++ fiber-api/server.go
@@ -1,18 +1,40 @@
 package main
 
 import (
+	"fmt"
+
 	fiber "github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	app := fiber.New()
+
 	app.Use(logger.New())
+
 	app.Get("/books", getBooks)
 	app.Get("/books/:id", getBookById)
 	app.Post("/books", createBook)
 	app.Patch("/books/:id", updateBook)
 	app.Delete("/books/:id", deleteBook)
 
+	app.Post("/upload", uploadFile)
+
 	app.Listen(":8080")
+}
+
+func uploadFile(c *fiber.Ctx) error {
+	file, err := c.FormFile("image")
+	fmt.Println(file, err)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+	c.SaveFile(file, "./uploads/"+file.Filename)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+
+	return c.SendString("File uploaded successfully!")
 }
